8409dc15a54de8869ad85052056224fcb46dccb8
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2023-07-29 13:00:28 +0330

    up command

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,14 @@ application {
     mainClass.set("com.chackavack.App")
 }
 
-tasks.named<Test>("test") {
+
+
+tasks.test{
     // Use TestNG for unit tests.
-    useTestNG()
-     testLogging {
+    useTestNG(){
+        // testLogging.showStandardStreams = true
+    }
+    testLogging {
         events("passed","skipped","failed")
     }
 }
